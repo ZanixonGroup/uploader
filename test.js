796ff8aef74427407ed1fe95ceb72d80eea43f50
ZTRdiamond <fatahillahalmakarim226@gmail.com>
2025-02-05 00:02:59 +0700

    add(cdn): shojib cdn

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import { QuaxUploader } from "./src/index.js";
+import { ShojibUploader } from "./src/index.js";
 
 const eg = (await axios.get("https://telegra.ph/file/3838948e59859e543df1d.jpg", {
   responseType: "arraybuffer"
 })).data;
 
-QuaxUploader(eg).then(console.log).catch(console.log)
+ShojibUploader(eg).then(console.log).catch(console.log)
