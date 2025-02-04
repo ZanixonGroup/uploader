6d0e3c37b8836c13ffb7b960aadd039f31420914
ZTRdiamond <fatahillahalmakarim226@gmail.com>
2025-02-05 00:13:55 +0700

    add(cdn): videy cdn

---
+++ src/services/videy.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import FormData from "form-data";
+import { FileSize, MimeType } from "./../utils/helper.js";
+
+export async function VideyUploader(buffer) {
+  if(!Buffer.isBuffer(buffer)) throw Error('Invalid buffer input!');
+  if((await FileSize(buffer)) >= 1048576000) throw Error('Max size upload for VideyCDN is only 1GB!');
+  
+  const mime = await MimeType(buffer);
+  if(!["mp4","mov"].includes(mime.ext)) throw Error('Wrong filetype, please select an .mp4 or .mov')
+  
+  const form = new FormData();
+  form.append("file", Buffer.from(buffer), {
+    filename: "zxn-" + Date.now() + "." + mime.ext,
+    contentType: mime.mime
+  });
+  const raw = await axios.post("https://videy.co/api/upload", form, {
+    headers: {
+      ...form.getHeaders()
+    }
+  });
+  
+  return "https://cdn.videy.co/" + raw?.data?.id + "." + mime.ext || null;
+}
--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 import axios from "axios";
-import { ShojibUploader } from "./src/index.js";
+import { VideyUploader } from "./src/index.js";
 
-const eg = (await axios.get("https://telegra.ph/file/3838948e59859e543df1d.jpg", {
+let video = "https://cdn.videy.co/eawVNAOu1.mp4",
+ image = "https://telegra.ph/file/3838948e59859e543df1d.jpg"
+const eg = (await axios.get(video, {
   responseType: "arraybuffer"
 })).data;
 
-ShojibUploader(eg).then(console.log).catch(console.log)
+VideyUploader(eg).then(console.log).catch(console.log)
