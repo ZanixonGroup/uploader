e679c3d2fa470810f94610e5f531511f51962504
ZTRdiamond <fatahillahalmakarim226@gmail.com>
2025-02-04 23:58:20 +0700

    add(cdn): quax cdn

--- test.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { PomfUploader } from "./src/index.js";
-
-const eg = (await axios.get("https://telegra.ph/file/3838948e59859e543df1d.jpg", {
-  responseType: "arraybuffer"
-})).data;
-
-PomfUploader(eg).then(console.log).catch(console.log)
