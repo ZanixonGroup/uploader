3c6aa7cddbfe7ce7f9ac24b48a8021ac2ff0aa3b
erhabot <erhabot@gmail.com>
2025-02-05 20:40:44 +0800

    add(cdn): erha cdn

---
+++ src/services/erhabot.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import FormData from "form-data";
+import { FileSize, MimeType } from "./../utils/helper.js";
+
+export async function ErhaImgUploader(buffer) {
+  if(!Buffer.isBuffer(buffer)) throw Error('Invalid buffer input!');
+  if((await FileSize(buffer)) >= 5242880) throw Error('The maximum upload size for images is 5 MB!');
+  
+  const mime = await MimeType(buffer);
+  if(!["jpg","jpeg","png"].includes(mime.ext)) throw Error('Only supports images!')
+  
+  const form = new FormData();
+  form.append("file", Buffer.from(buffer), {
+    filename: "zxn-" + Date.now() + "." + mime.ext,
+    contentType: mime.mime
+  });
+  const raw = await axios.post("https://cdn.erhabot.com/upload", form, {
+    headers: {
+      ...form.getHeaders()
+    }
+  });
+  
+  return raw?.data?.url || null;
+}
