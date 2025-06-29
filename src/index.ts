b0e9cefd6af6a457232fb4defc21c328cb949228
ZTRdiamond <fatahillahalmakarim226@gmail.com>
2025-06-29 10:30:56 +0700

    remove: erhabot cdn, service cannot be accessed

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,22 @@
 export * from "./services/catbox";
-export * from "./services/erhabot";
 export * from "./services/fasturl";
 export * from "./services/litterbox";
 export * from "./services/pomf";
 export * from "./services/quax";
 export * from "./services/ryzumi";
 export * from "./services/shojib";
-export * from "./services/tmperhabot";
 export * from "./services/uguu";
 export * from "./services/videy";
 export * from "./services/idnet";
 export * from "./services/cloudku";
 
 import * as Catbox from "./services/catbox";
-import * as Erhabot from "./services/erhabot";
 import * as FastUrl from "./services/fasturl";
 import * as Litterbox from "./services/litterbox";
 import * as Pomf from "./services/pomf";
 import * as Quax from "./services/quax";
 import * as Ryzumi from "./services/ryzumi";
 import * as Shojib from "./services/shojib";
-import * as TmpErhabot from "./services/tmperhabot";
 import * as Uguu from "./services/uguu";
 import * as Videy from "./services/videy";
 import * as IDNet from "./services/idnet";
@@ -28,14 +24,12 @@ import * as Cloudku from "./services/cloudku";
 
 export default {
   Catbox,
-  Erhabot,
   FastUrl,
   Litterbox,
   Pomf,
   Quax,
   Ryzumi,
   Shojib,
-  TmpErhabot,
   Uguu,
   Videy,
   IDNet,
--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,6 @@
 import fs from "fs/promises";
 import { dirname } from "desm";
-import Unit from "./../src/services/fasturl.ts";
+import Unit from "./../src/services/erhabot.ts";
 
 const __dirname = dirname(import.meta.url);
 
